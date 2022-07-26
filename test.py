875fc265881365692bb50c58bae3fff3e4e83beb
sulal <sulalakbar5@gmail.com>
2022-07-25 21:30:59 -0700

    Pract github

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
 import unittest
-
-#Doing some changes .......
-class TestSum(unittest.TestCase):
-
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 4)), 6, "Should be 6")
+import calc
 
 
-print("Added New Feature to test github/ Checking for conflict")
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        result = calc.add(10,5)
+        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(-1,-1),-2)
+
 
 if __name__ == '__main__':
     unittest.main()
